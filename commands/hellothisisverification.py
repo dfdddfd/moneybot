fda938db52a5edc909c4c1e8bbd8fe05e5ed2b11
dfdddfd <60836631+dfdddfd@users.noreply.github.com>
2020-08-30 11:54:24 +0900

    Update hellothisisverification.py

--- commands/hellothisisverification.py
+++ commands/hellothisisverification.py
@@ -2,4 +2,4 @@ class Command:
     name = 'hellothisisverification'
 
     async def run(self, client, message, ext):
-        await message.channel.send('WHAT ARE YOU?#6367(617286714615922698)')
+        await message.channel.send('코드 사용자#태그(ID)')
